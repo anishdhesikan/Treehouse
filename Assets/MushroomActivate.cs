bdf0df5b7cbef960470e7d8bf90dce4e1f9cb4df
Anish Dhesikan <anishdhesikan@gmail.com>
2016-10-12 18:52:58 -0400

    Change controls and add some environmental effects

---
+++ Assets/MushroomActivate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MushroomActivate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (col.transform.tag == "Snowman") {
+			Application.LoadLevel (1);
+		}
+	}
+}
--- Assets/PlaceCube.cs
+++ Assets/PlaceCube.cs
@@ -5,9 +5,6 @@ using UnityEngine.Networking;
 public class PlaceCube : NetworkBehaviour {
 
 	public GameObject cubePrefab;
-	public GameObject giftPrefab;
-
-	public GameObject giftSpawnPoint;
 
 	// Use this for initialization
 	void Start () {
@@ -24,19 +21,5 @@ public class PlaceCube : NetworkBehaviour {
 				NetworkServer.Spawn(cube);
 			}
 		}
-
-		if (Input.GetMouseButtonDown(1)) {
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit = new RaycastHit ();
-			if (Physics.Raycast(ray, out hit, 1000f)) {
-				GameObject gift = (GameObject)Instantiate(giftPrefab, hit.point + Vector3.up, Quaternion.identity);
-				NetworkServer.Spawn(gift);
-			}
-		}
-	}
-
-	public void PlaceGift () {
-		GameObject gift = (GameObject)Instantiate(giftPrefab, giftSpawnPoint.transform.position, Quaternion.identity);
-		NetworkServer.Spawn(gift);
 	}
 }
